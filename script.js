e301c20f9c8d6ce996a17972973823017beaa055
Arporonga <franco290415@gmail.com>
2024-07-31 14:01:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', function() {
-    const slider = document.querySelector('.slider-background');
-    let currentIndex = 0;
+    const slides = document.querySelector('.slides');
+    let index = 0;
 
-    function updateSlider() {
-        if (currentIndex === 0) {
-            slider.style.backgroundImage = 'url("panel.jpg")';
-        } else {
-            slider.style.backgroundImage = 'url("panel4.jpg")';
-        }
-    }
-
-    // Inicializa el slider
-    updateSlider();
-
-    setInterval(function() {
-        currentIndex = (currentIndex + 1) % 2;
-        updateSlider();
-    }, 4000); // Cambiar cada 4 segundos
+    setInterval(() => {
+        index = (index + 1) % 2;
+        slides.style.transform = `translateX(-${index * 50}%)`;
+    }, 5000);
 });
