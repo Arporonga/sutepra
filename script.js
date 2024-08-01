fd0aa21236759bfb03511c83be0e96c1286e70f3
Arporonga <franco290415@gmail.com>
2024-08-01 07:48:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,31 @@
 // script.js
-document.addEventListener('DOMContentLoaded', function() {
-    // Slider functionality
-    const slides = document.querySelector('.slides');
-    let currentIndex = 0;
-    const slideCount = document.querySelectorAll('.slide').length;
-    const slideWidth = document.querySelector('.slide').offsetWidth;
+document.addEventListener('DOMContentLoaded', function () {
+    // Ordenar productos
+    const orderButton = document.getElementById('order-button');
+    const dropdownContent = document.querySelector('.dropdown-content');
+    const productosContainer = document.getElementById('productos-container');
 
-    function showSlide(index) {
-        slides.style.transform = `translateX(-${index * slideWidth}px)`;
-    }
-
-    setInterval(() => {
-        currentIndex = (currentIndex + 1) % slideCount;
-        showSlide(currentIndex);
-    }, 3000);
-
-    // Ordenar por precio
-    document.getElementById('order-button').addEventListener('click', function() {
-        const dropdown = document.querySelector('.dropdown-content');
-        dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+    orderButton.addEventListener('click', function () {
+        dropdownContent.classList.toggle('show');
     });
 
-    document.getElementById('sort-low-to-high').addEventListener('click', function() {
+    document.getElementById('sort-low-to-high').addEventListener('click', function () {
         sortProducts('asc');
     });
 
-    document.getElementById('sort-high-to-low').addEventListener('click', function() {
+    document.getElementById('sort-high-to-low').addEventListener('click', function () {
         sortProducts('desc');
     });
 
     function sortProducts(order) {
-        const container = document.getElementById('productos-container');
-        const products = Array.from(container.getElementsByClassName('producto'));
-        products.sort((a, b) => {
+        const productos = Array.from(productosContainer.getElementsByClassName('producto'));
+
+        productos.sort((a, b) => {
             const priceA = parseFloat(a.getAttribute('data-price'));
             const priceB = parseFloat(b.getAttribute('data-price'));
             return order === 'asc' ? priceA - priceB : priceB - priceA;
         });
-        products.forEach(product => container.appendChild(product));
+
+        productos.forEach(producto => productosContainer.appendChild(producto));
     }
 });
